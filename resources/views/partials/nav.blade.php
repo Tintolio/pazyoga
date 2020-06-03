a17ab601550a71da3cd63e8d760e06f69f8f5897
robets2294 <robets2294@gmail.com>
2020-06-03 11:22:24 -0400

    login

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,25 @@
+<nav class="nav-menu d-none d-lg-block">
+  <ul>
+    <li class="active"><a href="#header">Home</a></li>
+    <li class="drop-down"><a href="">yoga 1</a>
+  <ul>
+  
+  <li><a href="#about">yoga 2</a></li>
+  <li><a href="#team">Team</a></li>
+  <li class="drop-down"><a href="#">Seccion</a>
+
+  <ul>
+    <li><a href="#">1</a></li>
+    <li><a href="#">2</a></li>
+    <li><a href="#">3</a></li>
+    <li><a href="#">4</a></li>
+    <li><a href="#">5</a></li>
+  </ul>
+              </li>
+            </ul>
+          </li>
+          <li><a href="#services">Servicios</a></li>
+          <li><a href="#contact">Contacto</a></li>
+
+        </ul>
+</nav><!-- .nav-menu -->
